3801f673c62d2d0abc220b4eba5245c8efb5c1fa
Vera de Kok <mail@veradekok.nl>
2026-07-03 00:44:17 +0200

    Desktop acceleration on hold, step=1 on all inputs

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('croptool', ['LocalStorageModule', 'ngSanitize', 'ui.bootstrap', 'angular-ladda', 'pascalprecht.translate','ngTouch']).
+angular.module('croptool', ['LocalStorageModule', 'ngSanitize', 'ui.bootstrap', 'angular-ladda', 'pascalprecht.translate']).
 
 config(['$translateProvider', function($translateProvider) {
     $translateProvider.useSanitizeValueStrategy('escapeParameters');
@@ -1911,4 +1911,3 @@ controller('AppCtrl', ['$scope', '$http', '$timeout', '$q', '$window', '$httpPar
     });
 
 }]);
-
